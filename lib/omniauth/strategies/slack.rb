e2f281e131b40d674717303c35c9b9f036cb400c
Brandon Hilkert <brandonhilkert@gmail.com>
2017-08-24 20:12:23 -0400

    Prefer redirect_url over script

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -101,7 +101,7 @@ module OmniAuth
       private
 
       def callback_url
-        full_host + script_name + callback_path
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
     end
   end
